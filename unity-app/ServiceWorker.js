a5e3b9aab9a873d5b4b388bce04249a8552290e4
TonyMac129 <tonyforself@gmail.com>
2024-12-04 20:43:33 -0500

    FINAL UNITY BUILD UPLOAD!!!!
    APP COMPLETED!!!!!!

---
+++ unity-app/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-sharing project-0.1";
+const contentToCache = [
+    "Build/unity-build-test-3.loader.js",
+    "Build/unity-build-test-3.framework.js",
+    "Build/unity-build-test-3.data",
+    "Build/unity-build-test-3.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
